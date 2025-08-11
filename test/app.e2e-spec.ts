a9adc86993d2cd242a0139f74184bfe4cfa3c898
Blueberry labs <luu@blueberrylabs.com>
2025-08-11 20:39:57 +0700

    fix build

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from './../src/app.module';
+import type { Server } from 'http';
 
 interface UserResponse {
   id: string;
@@ -30,12 +31,12 @@ describe('UserController (e2e)', () => {
   });
 
   it('should register a user', async () => {
-    const res = await request(app.getHttpServer() as any)
+    const res = await request(app.getHttpServer() as unknown as Server)
       .post('/users/register')
       .send({ email: 'e2e@example.com', password: 'pass', name: 'E2E' })
       .expect(201);
 
-    const user: UserResponse = res.body as UserResponse;;
+    const user: UserResponse = res.body as UserResponse;
 
     expect(user).toHaveProperty('id');
     expect(user.email).toBe('e2e@example.com');
@@ -43,20 +44,20 @@ describe('UserController (e2e)', () => {
 
   it('should get a user by id', async () => {
     // First, register a user
-    const registerRes = await request(app.getHttpServer() as any)
+    const registerRes = await request(app.getHttpServer() as unknown as Server)
       .post('/users/register')
       .send({ email: 'e2e2@example.com', password: 'pass', name: 'E2E2' })
       .expect(201);
 
-    const registeredUser: UserResponse = registerRes.body as UserResponse;;
+    const registeredUser: UserResponse = registerRes.body as UserResponse;
     const userId = registeredUser.id;
 
     // Then, get the user
-    const getRes = await request(app.getHttpServer() as any)
+    const getRes = await request(app.getHttpServer() as unknown as Server)
       .get(`/users/${userId}`)
       .expect(200);
 
-    const user: UserResponse = getRes.body as UserResponse;;
+    const user: UserResponse = getRes.body as UserResponse;
     expect(user).toHaveProperty('id', userId);
     expect(user.email).toBe('e2e2@example.com');
   });
